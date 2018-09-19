03d1378a2a9aa8bfaddf22accfb50250b7b8a519
Dhaneesh.GK <dhaneeshatservices@gmail.com>
2018-09-19 10:54:13 +0530

    changes to not found

--- web_js/Rest App/lib/handlers/not-found.js
+++ web_js/Rest App/lib/handlers/not-found.js
@@ -3,7 +3,7 @@
 // Not found handler
 notFound = (data,callback)=>{
     // console.log("user is ")
-    callback(404)
+    callback(404, {"message":"are you are wandering? check the links","links":["https://www.google.com/","https://www.youtube.com/"]})
 }
 
 module.exports = {notFound}
